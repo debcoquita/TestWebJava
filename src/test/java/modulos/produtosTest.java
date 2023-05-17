649dc15fd2114e3c892d0ace7af3cb4655c6fde6
unknown <debora_marine@hotmail.com>
2023-05-16 22:11:37 -0300

    segunda parte do esboço da automação de teste web com java

--- src/test/java/modulos/produtosTest.java
+++ src/test/java/modulos/produtosTest.java
@@ -1,6 +1,10 @@
 package modulos;
 
+import java.time.Duration;
+
+import org.asynchttpclient.util.Assertions;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,12 +19,34 @@ public class produtosTest {
         WebDriver navegador = new ChromeDriver();
         //maximizar 
         navegador.manage().window().maximize();
+        //Tempo de espera padrão
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         //navegar na home page
-        navegador.get("htpp://165.227.93.41/lojinha-web/v2/");
+        navegador.get("http://165.227.93.41/lojinha-web/v2/");
         //fazer login
-        
+        navegador.findElement(By.cssSelector("label[for='usuario']")).click();
+        navegador.findElement(By.id("usuario")).sendKeys("admin");
+        navegador.findElement(By.cssSelector("label[for='senha']")).click();
+        navegador.findElement(By.id("senha")).sendKeys("admin");
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+
+        //tela de registro de produtos
+        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
+
+        //preenchendo os dados do produto
+        navegador.findElement(By.id("produtonome")).sendKeys("Oculos");
+        navegador.findElement(By.id("produtovalor")).sendKeys("000");
+        navegador.findElement(By.name("produtocores")).sendKeys("preto com astes prata");
+
         //registrar produto com valor zero
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+        //validar mensagem de erro
+        //<div class="toast rounded" style="top: 0px; transition: transform 0.2s ease 0s, opacity 0.2s ease 0s;">O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00</div>
+        final String mensagemToast = navegador.findElement(By.cssSelector(".toast.rounded")).getText();
+        Assertions.assertNotEmpty("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToast);
+        
         //fechar browser
+        navegador.quit();
     }
 
 
